ca6be838e0c6c9078ad5673d7c1237509e0df0be
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2026-02-09 16:04:08 +0600

    Create player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    
+    // Movement-related
+    private float _movementX;
+    private float _movementY;
+    public float speed;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        _movementX = movementValue.Get<Vector2>().x;
+        _movementY = movementValue.Get<Vector2>().y;
+    }
+    void FixedUpdate()
+    {
+        // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3 (_movementX, 0.0f, _movementY) * speed;
+        _rigidbody.linearVelocity = movement;
+        _rigidbody.linearVelocity = Vector3.Lerp(_rigidbody.linearVelocity, movement, 0.2f);
+
+        
+    }
+}
