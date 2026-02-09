d4c103ad6a2fb494beba5b58b4ad4b582c4aac2a
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2026-02-09 17:19:10 +0600

    Refactor shop and implement rudimentary shopping functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -24,10 +25,26 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        // Create a 3D movement vector using the X and Y inputs.
         Vector3 movement = new Vector3 (_movementX, 0.0f, _movementY) * speed;
         _rigidbody.linearVelocity = movement;
+    }
 
-        
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Buy") || collision.gameObject.CompareTag("Sell") )
+        {
+            ShopItemDetails details = collision.gameObject.GetComponentInParent<ShopItemDetails>();
+            float price = details.price;
+            string itemName = details.itemName;
+            
+            if (collision.gameObject.CompareTag("Buy"))
+            {
+                Debug.Log("Bought " + itemName + " for " + price);
+            }
+            else
+            {
+                Debug.Log("Sold " + itemName + " for " + price);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/ShopItemDetails.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class ShopItemDetails : MonoBehaviour
+{
+    public string itemName = "Unnamed Item";
+    public float price = 10;
+}
